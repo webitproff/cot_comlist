beb78e180ce054fb48756fff4c4060a0746cbb5e
Quasiocculti <q@sed.by>
2023-09-19 18:57:23 +0300

    PHP 8.1 fix #2

--- comlist/comlist.first.black_white.php
+++ comlist/comlist.first.black_white.php
@@ -15,6 +15,8 @@ Hooks=comlist.first
 
 defined('COT_CODE') or die('Wrong URL');
 
+$bw_mode = false;
+
 if (strpos($extra, ';')) {
   $bw_mode = (substr($extra, 0, 1) == "+") ? 'array_white' : 'array_black';
   $bw_cats = substr($extra, 2);
