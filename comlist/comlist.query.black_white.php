5e0502c04b1e8cf6aff9efc8b79de1f841c314a2
Quasiocculti <q@sed.by>
2023-10-02 11:54:30 +0300

    Kabak fix

--- comlist/comlist.query.black_white.php
+++ comlist/comlist.query.black_white.php
@@ -15,7 +15,7 @@ Hooks=comlist.query
 
 defined('COT_CODE') or die('Wrong URL');
 
-require_once cot_incfile('pagelist', 'plug');
+require_once cot_incfile('cotlib', 'plug');
 
 if ($bw_mode) {
   $bw_cats = explode(';', $bw_cats);
